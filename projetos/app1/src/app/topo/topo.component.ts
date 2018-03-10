4b62431151b2f9257a15ba404b9ef17689678a13
heliohvbj <heliohvbj@gmail.com>
2018-03-09 21:39:01 -0300

    criando componente

---
+++ projetos/app1/src/app/topo/topo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-topo',
+    //templateUrl: './topo.component.html'
+    template: `<h1>Aprendendo template inline</h1>
+    <p>template</p>`
+})
+export class TopoComponent {}
