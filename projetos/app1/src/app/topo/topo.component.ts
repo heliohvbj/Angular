18d0ac4aca738a2ad417106882a460e17e548cc7
heliohvbj <heliohvbj@gmail.com>
2018-09-16 22:26:33 -0300

    adicionando novos componentes

--- projetos/app1/src/app/topo/topo.component.ts
+++ projetos/app1/src/app/topo/topo.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'app-topo',
-    //templateUrl: './topo.component.html'
-    template: `<h1>Aprendendo template inline</h1>
-    <p>template</p>`
+    templateUrl: './topo.component.html',
+    //template: `<h1 class='test'>Aprendendo template inline</h1>
+    //<p>template</p>`,
+    styleUrls: ['./topo.component.css'],
+    //styles: [`.test{ color: red }`]
 })
 export class TopoComponent {}
