4b2edfce478e0b61d669e448be9345c8681aeb76
heliohvbj <heliohvbj@gmail.com>
2018-09-18 22:35:01 -0300

    Adicionando eventos

--- projetos/app1/src/app/painel/painel.component.ts
+++ projetos/app1/src/app/painel/painel.component.ts
@@ -10,9 +10,52 @@ import {FRASES} from './frases-mock';
 export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
-  constructor() { console.log(this.frases)}
+  public instrucao: string ='Traduza a frase:'
+  public resposta: string
+  public rodada: number = 0
+  public rodadaFrase: Frase
+  public progresso: number = 0
+
+  constructor() { 
+    console.log(this.frases)
+    this.rodadaFrase = this.frases[this.rodada]
+  }
+
 
   ngOnInit() {
   }
 
+  public atualizaResposta(resposta: Event) : void
+  {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+  }
+
+  public verificarResposta() : void
+  {
+   
+    if(this.resposta == this.rodadaFrase.frasePtBr)
+      this.respostaCerta()
+    else
+      this.respostaErrada()
+  }
+
+  public respostaCerta() : void
+  {
+    this.rodada++
+    this.progresso = this.progresso + (100 / this.frases.length)
+
+    console.log(this.progresso);
+    
+    if(this.rodada >= 4)
+    {
+      this.rodada  = 0
+      this.progresso = 0
+    }
+      this.rodadaFrase = this.frases[this.rodada]
+  }
+
+  public respostaErrada() : void
+  {
+    alert("A tradução está errada")
+  }
 }
